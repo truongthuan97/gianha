77c940d6a4479cc6912ff3712944b1def380f7b2
hứa văn dũng <60870815+dunghuava@users.noreply.github.com>
2020-08-01 09:57:19 +0700

    add database

--- application/controllers/admin/Dasboard.php
+++ application/controllers/admin/Dasboard.php
@@ -3,8 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dasboard extends MY_Controller {
 
+public function __construct()
+{
+	parent::__construct();
+	$this->load->model('Project_M');
+	
+}
 	public function index()
 	{
+		$all_prj = $this->Project_M->all();
+		print_r($all_prj);die;
+
 		$this->get_admin_header();
 		$this->page('dasboard/dasboard',$this->data);
 		$this->get_admin_footer();
---
+++ application/models/Project_M.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Project_M extends MY_model {
+	protected $table = 'db_project';
+}
+
+/* End of file Project_M.php */
+/* Location: ./application/models/Project_M.php */
